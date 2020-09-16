e7b48cad909284a38cfb845dda3c0b1e96e3e9a0
Jordi Gomper <jordigomper@gmail.com>
2020-09-16 17:17:07 +0200

    check out of time

--- src/Meeting/Interactors/createMeeting.interactor.ts
+++ src/Meeting/Interactors/createMeeting.interactor.ts
@@ -1,9 +1,12 @@
+import { DateTime } from "luxon";
 import IMeeting from "../Domain/Meeting.interface";
 import MeetingRepository from "../Domain/Meeting.repository";
 
 const saveMeeting = (
   meetingRepository: MeetingRepository
 ) => async (meeting: IMeeting): Promise<boolean> => {
+  const {startAt, finishAt} = meeting;
+  if(DateTime.fromISO(startAt, { setZone: true }).hour < 9 || DateTime.fromISO(finishAt, { setZone: true }).hour  > 18) throw 'No se pueden crear reuniónes fuera del rango establecido';
   return meetingRepository.save(meeting);
 };
 
--- src/Meeting/__test__/Meeting.spec.tsx
+++ src/Meeting/__test__/Meeting.spec.tsx
@@ -21,7 +21,52 @@ describe("Meeting business requirements", () => {
     };
     expect(await saveMeeting(MeetingRepositoryMock)(meeting)).toBeTruthy();
   }); 
-  // xit('El horario laboral es de 9:00 hasta las 18:00. Ninguna reunion puede empezar antes o acabar después de esas horas', () => {});
+  describe('El horario laboral es de 9:00 hasta las 18:00. Ninguna reunion puede empezar antes o acabar después de esas horas', () => {
+    it('debería dar error si la hora de inicio es menor a las 9A.M.', async (done) => {
+      const assistants: IUser = {
+        id: 'fake-id',
+        name: 'fake-name',
+      };
+      const meeting: IMeeting = {
+        id: 'fake-meeting-id',
+        name: 'fake-meeting-name',
+        startAt: '2020-10-13T08:00:00.000Z',
+        finishAt: '2020-10-13T11:00:00.000Z',
+        assistants: [assistants],
+      };
+      const MeetingRepositoryMock: MeetingRepository = {
+        save: (meeting: IMeeting) => Promise.resolve(true),
+      };
+      try {
+        await saveMeeting(MeetingRepositoryMock)(meeting);
+      } catch (error) {
+        expect(error).toEqual('No se pueden crear reuniónes fuera del rango establecido');
+        done();
+      }
+    });
+    it('debería dar error si la hora de finalziación es mayor a las 6P.M.', async (done) => {
+      const assistants: IUser = {
+        id: 'fake-id',
+        name: 'fake-name',
+      };
+      const meeting: IMeeting = {
+        id: 'fake-meeting-id',
+        name: 'fake-meeting-name',
+        startAt: '2020-10-13T10:00:00.000Z',
+        finishAt: '2020-10-13T19:00:00.000Z',
+        assistants: [assistants],
+      };
+      const MeetingRepositoryMock: MeetingRepository = {
+        save: (meeting: IMeeting) => Promise.resolve(true),
+      };
+      try {
+        await saveMeeting(MeetingRepositoryMock)(meeting);
+      } catch (error) {
+        expect(error).toEqual('No se pueden crear reuniónes fuera del rango establecido');
+        done();
+      }
+    });
+  });
   // xit('El momento de inicio de una reunión debe ser siempre estrictamente inferior que el momento de finalización de la misma reunión', () => {});
   // xit('Una reunión debe empezar y terminar en el mismo día (no pueden crearse reuniones que empiecen en el día i y terminen en el día i+1)', () => {});
   // xit('Una reunión tiene que ser atendida por mínimo un usuario, sin límite de participantes', () => {});
