59db1e28aa930dc13b67246ece4bd73c9483adf4
Jordi Gomper <jordigomper@gmail.com>
2020-09-16 17:29:12 +0200

    convert params to datatime

--- src/Meeting/Interactors/createMeeting.interactor.ts
+++ src/Meeting/Interactors/createMeeting.interactor.ts
@@ -5,8 +5,9 @@ import MeetingRepository from "../Domain/Meeting.repository";
 const saveMeeting = (
   meetingRepository: MeetingRepository
 ) => async (meeting: IMeeting): Promise<boolean> => {
-  const {startAt, finishAt} = meeting;
-  if(DateTime.fromISO(startAt, { setZone: true }).hour < 9 || DateTime.fromISO(finishAt, { setZone: true }).hour  > 18) throw 'No se pueden crear reuniónes fuera del rango establecido';
+  const startAtISODataTime = DateTime.fromISO(meeting.startAt, { setZone: true });
+  const finishAtISODataTime = DateTime.fromISO(meeting.finishAt, { setZone: true });
+  if(startAtISODataTime.hour < 9 || finishAtISODataTime.hour  > 18) throw 'No se pueden crear reuniónes fuera del rango establecido';
   return meetingRepository.save(meeting);
 };
 
