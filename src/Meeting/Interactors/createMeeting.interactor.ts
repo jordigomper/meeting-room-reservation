90a0e51c64cf625dbdfa9d28e97477254f20e547
Jordi Gomper <jordigomper@gmail.com>
2020-09-17 23:19:32 +0200

    refactor

--- src/Meeting/Interactors/createMeeting.interactor.ts
+++ src/Meeting/Interactors/createMeeting.interactor.ts
@@ -8,36 +8,27 @@ const saveMeeting = (
 ) => async (meeting: IMeeting): Promise<boolean> => {
   const startAt: DateTime = parseToISODataTime(meeting.startAt);
   const finishAt: DateTime = parseToISODataTime(meeting.finishAt);
-  checkBusinessRulesOfMeetingTimeFormat(startAt, finishAt);
-  await checkBusinessRulesOfUsers(meeting.assistants, meetingRepository, startAt, finishAt);
-  return meetingRepository.save(meeting);
-};
+  const {assistants} = meeting;
 
-export default saveMeeting;
-
-async function checkBusinessRulesOfUsers(users: IUser[], meetingRepository: MeetingRepository, startAt: DateTime, finishAt: DateTime) {
-  if (haveUsers(users))
-    throw 'Se ha de añadir mínimo un participante en la reunión.';
-
-  const meetingsByUser: IMeeting[] = await meetingRepository.getMeetingByUsers(users);
-
-  meetingsByUser.map((meeting: IMeeting) => {
-    const meetingStartDataTime = parseToISODataTime(meeting.startAt);
-    const meetingFinishDataTime = parseToISODataTime(meeting.finishAt);
-    if (isMeetingsInSameTime(startAt, finishAt, meetingStartDataTime, meetingFinishDataTime)) {
-      throw 'Hay usuarios que no tiene disponibilidad para esta reunión.';
-    }
-  });
-}
-
-function checkBusinessRulesOfMeetingTimeFormat(startAt: DateTime, finishAt: DateTime) {
+  // format time of meeting constraints
   if (isOutTime(startAt, finishAt))
     throw 'No se pueden crear reuniónes fuera del rango establecido';
   if (isTheMinimumMeetingTimeInsufficient(startAt, finishAt))
     throw 'La duración mínima de la reunión debe de ser de 30 minutos.';
   if (!isSameDay(startAt, finishAt))
     throw 'La reunión no puede exceder de un día.';
-}
+
+  // user constraints
+  if (haveUsers(assistants))
+    throw 'Se ha de añadir mínimo un participante en la reunión.';
+  const meetingsByUser: IMeeting[] = await meetingRepository.getMeetingByUsers(assistants);
+  if(areThereUsersUnavailable(meetingsByUser, startAt, finishAt)) 
+    throw 'Hay usuarios que no tiene disponibilidad para esta reunión.';
+
+  return meetingRepository.save(meeting);
+};
+
+export default saveMeeting;
 
 /**
  * 
@@ -72,4 +63,12 @@ function timeOverlaps(startAtDate1, finishAtDate1, startAtDate2, finishAtDate2) 
   && finishAtDate2.diff(finishAtDate1, 'hour').hours < 0
   || startAtDate1.diff(startAtDate2, 'hour').hours > 0
   || finishAtDate2.diff(finishAtDate1, 'hour').hours > 0;
+}
+
+function areThereUsersUnavailable(meetingsByUser: IMeeting[], startAt: DateTime, finishAt: DateTime): boolean {
+  return meetingsByUser.some((meeting: IMeeting) => {
+    const meetingStartDataTime = parseToISODataTime(meeting.startAt);
+    const meetingFinishDataTime = parseToISODataTime(meeting.finishAt);
+    return isMeetingsInSameTime(startAt, finishAt, meetingStartDataTime, meetingFinishDataTime);
+  });
 }
