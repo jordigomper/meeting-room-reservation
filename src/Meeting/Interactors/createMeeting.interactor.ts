334b85579afa3492a5a228bfd2039663b97d6d89
Jordi Gomper <jordigomper@gmail.com>
2020-09-17 17:35:58 +0200

    refactor naming and typing

--- src/Meeting/Interactors/createMeeting.interactor.ts
+++ src/Meeting/Interactors/createMeeting.interactor.ts
@@ -1,31 +1,69 @@
 import { DateTime } from "luxon";
+import IUser from "../../User/Domain/User.interface";
 import IMeeting from "../Domain/Meeting.interface";
 import MeetingRepository from "../Domain/Meeting.repository";
 
 const saveMeeting = (
   meetingRepository: MeetingRepository
 ) => async (meeting: IMeeting): Promise<boolean> => {
-  const startAtISODataTime = DateTime.fromISO(meeting.startAt, { setZone: true });
-  const finishAtISODataTime = DateTime.fromISO(meeting.finishAt, { setZone: true });
-  const {assistants} = meeting;
-  if(startAtISODataTime.hour < 9 || finishAtISODataTime.hour  > 18) throw 'No se pueden crear reuniónes fuera del rango establecido';
-  if(finishAtISODataTime.diff(startAtISODataTime, 'minute').values.minutes < 30) throw 'La duración mínima de la reunión debe de ser de 30 minutos.';
-  if(finishAtISODataTime.diff(startAtISODataTime, 'day').values.days > 1) throw 'La duración máxima de una reunión es de 8 horas.';
-  if(!assistants.length) throw 'Se ha de añadir mínimo un participante en la reunión.';
-  const meetingsByUser: IMeeting[] = await meetingRepository.getMeetingByUser(assistants);
-  meetingsByUser.map((meeting: IMeeting) => {
-    const meetingStartDataTime = DateTime.fromISO(meeting.startAt, { setZone: true });
-    const meetingFinishDataTime = DateTime.fromISO(meeting.finishAt, { setZone: true });
-    if(
-        startAtISODataTime.diff(meetingStartDataTime, 'hour').values.hours < 0  
-        && meetingFinishDataTime.diff(finishAtISODataTime, 'hour').values.hours < 0 
-        || startAtISODataTime.diff(meetingStartDataTime, 'hour').values.hours > 0 
-        ||  meetingFinishDataTime.diff(finishAtISODataTime, 'hour').values.hours > 0
-      ) {
-      throw 'Hay usuarios que no tiene disponibilidad para esta reunión.';
-    }
-  });
+  const startAtISODataTime: DateTime = DateTime.fromISO(meeting.startAt, { setZone: true });
+  const finishAtISODataTime: DateTime = DateTime.fromISO(meeting.finishAt, { setZone: true });
+  checkBusinessRulesOfMeetingTimeFormat(startAtISODataTime, finishAtISODataTime);
+  await checkBusinessRulesOfUsers(meeting.assistants, meetingRepository, startAtISODataTime, finishAtISODataTime);
   return meetingRepository.save(meeting);
 };
 
 export default saveMeeting;
+
+/**
+ * 
+ * Utils
+ */
+
+async function checkBusinessRulesOfUsers(assistants: IUser[], meetingRepository: MeetingRepository, startAtISODataTime: DateTime, finishAtISODataTime: DateTime) {
+  if (haveUsers(assistants))
+    throw 'Se ha de añadir mínimo un participante en la reunión.';
+
+  const meetingsByUser: IMeeting[] = await meetingRepository.getMeetingByUser(assistants);
+
+  meetingsByUser.map((meeting: IMeeting) => {
+    const meetingStartDataTime = DateTime.fromISO(meeting.startAt, { setZone: true });
+    const meetingFinishDataTime = DateTime.fromISO(meeting.finishAt, { setZone: true });
+
+    if (isMeetingsInSameTime(startAtISODataTime, finishAtISODataTime, meetingStartDataTime, meetingFinishDataTime)) {
+      throw 'Hay usuarios que no tiene disponibilidad para esta reunión.';
+    }
+  });
+}
+
+function checkBusinessRulesOfMeetingTimeFormat(startAtISODataTime: DateTime, finishAtISODataTime: DateTime) {
+  if (isOutTime(startAtISODataTime, finishAtISODataTime))
+    throw 'No se pueden crear reuniónes fuera del rango establecido';
+  if (isTheMinimumMeetingTimeInsufficient(finishAtISODataTime, startAtISODataTime))
+    throw 'La duración mínima de la reunión debe de ser de 30 minutos.';
+  if (isTheMaximumTimeExceeded(finishAtISODataTime, startAtISODataTime))
+    throw 'La duración máxima de una reunión es de 8 horas.';
+}
+
+function isMeetingsInSameTime(startAtISODataTime: DateTime, finishAtISODataTime: DateTime, meetingStartDataTime: DateTime, meetingFinishDataTime: DateTime) {
+  return startAtISODataTime.diff(meetingStartDataTime, 'hour').hours < 0
+    && meetingFinishDataTime.diff(finishAtISODataTime, 'hour').hours < 0
+    || startAtISODataTime.diff(meetingStartDataTime, 'hour').hours > 0
+    || meetingFinishDataTime.diff(finishAtISODataTime, 'hour').hours > 0;
+}
+
+function haveUsers(assistants: IUser[]) {
+  return !assistants.length;
+}
+
+function isTheMaximumTimeExceeded(finishAtISODataTime: DateTime, startAtISODataTime: DateTime) {
+  return finishAtISODataTime.diff(startAtISODataTime, 'day').days > 1;
+}
+
+function isTheMinimumMeetingTimeInsufficient(finishAtISODataTime: DateTime, startAtISODataTime: DateTime) {
+  return finishAtISODataTime.diff(startAtISODataTime, 'minute').minutes < 30;
+}
+
+function isOutTime(startAtISODataTime: DateTime, finishAtISODataTime: DateTime) {
+  return startAtISODataTime.hour < 9 || finishAtISODataTime.hour > 18;
+}
