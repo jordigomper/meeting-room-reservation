32d6d04152354f91f68ff010eef812a10a05b5c3
Jordi Gomper <jordigomper@gmail.com>
2020-09-21 00:37:59 +0200

    extract utils fn in to file

--- src/Meeting/Interactors/createMeeting.interactor.ts
+++ src/Meeting/Interactors/createMeeting.interactor.ts
@@ -1,7 +1,7 @@
 import { DateTime } from "luxon";
-import IUser from "../../User/Domain/User.interface";
 import IMeeting from "../Domain/Meeting.interface";
 import MeetingRepository from "../Domain/Meeting.repository";
+import { parseToISODataTime, isOutTime, isTheMinimumMeetingTimeInsufficient, isSameDay, haveUsers, areThereUsersUnavailable } from "./utils/dataTimeMath";
 
 const saveMeeting = (
   meetingRepository: MeetingRepository
@@ -29,51 +29,3 @@ const saveMeeting = (
 };
 
 export default saveMeeting;
-
-/**
- * 
- * Utils
- */
-function parseToISODataTime(dataTime: string): DateTime {
-  return DateTime.fromISO(dataTime, { setZone: true });
-}
-
-function isMeetingsInSameTime(startAtDate1: DateTime, finishAtDate1: DateTime, startAtDate2: DateTime, finishAtDate2: DateTime) {
-  return isSameDay(startAtDate1, startAtDate2) && timeOverlaps(startAtDate1, finishAtDate1, startAtDate2, finishAtDate2);
-}
-
-function haveUsers(assistants: IUser[]) {
-  return !assistants.length;
-}
-
-function isTheMinimumMeetingTimeInsufficient(startAt: DateTime, finishAt: DateTime) {
-  return finishAt.diff(startAt, 'minute').minutes < 30;
-}
-
-function isOutTime(startAt: DateTime, finishAt: DateTime) {
-  return startAt.hour < 9 || finishAt.hour > 18;
-}
-
-function isSameDay(date1: DateTime, date2: DateTime): boolean {
-  return date1.hasSame(date2, 'day') && date1.hasSame(date2, 'month') && date1.hasSame(date2, 'year');
-}
-
-export function timeOverlaps(startAtDate1: DateTime, finishAtDate1: DateTime, startAtDate2: DateTime, finishAtDate2: DateTime): boolean {
-          // empieza misma hora
-  return startAtDate1.hasSame(startAtDate2, 'minute') 
-          || finishAtDate1.hasSame(finishAtDate2, 'minute')
-          // empieza antes acaba igual o después
-          || startAtDate2.diff(startAtDate1, 'hour').hours > 0
-          && startAtDate2.diff(finishAtDate1, 'hour').hours <= 0
-          // empieza en mitad de una reuniçon
-          || startAtDate2.diff(startAtDate1, 'hour').hours < 0
-          && finishAtDate2.diff(startAtDate1, 'hour').hours > 0;
-}
-
-function areThereUsersUnavailable(meetingsByUser: IMeeting[], startAt: DateTime, finishAt: DateTime): boolean {
-  return meetingsByUser.some((meeting: IMeeting) => {
-    const meetingStartDataTime = parseToISODataTime(meeting.startAt);
-    const meetingFinishDataTime = parseToISODataTime(meeting.finishAt);
-    return isMeetingsInSameTime(startAt, finishAt, meetingStartDataTime, meetingFinishDataTime);
-  });
-}
---
+++ src/Meeting/Interactors/utils/dataTimeMath.ts
@@ -0,0 +1,40 @@
+import { DateTime } from "luxon";
+import IUser from "../../../User/Domain/User.interface";
+import IMeeting from "../../Domain/Meeting.interface";
+
+export function parseToISODataTime(dataTime: string): DateTime {
+  return DateTime.fromISO(dataTime, { setZone: true });
+}
+export function haveUsers(assistants: IUser[]): boolean {
+  return !assistants.length;
+}
+export function isTheMinimumMeetingTimeInsufficient(startAt: DateTime, finishAt: DateTime): boolean {
+  return finishAt.diff(startAt, 'minute').minutes < 30;
+}
+export function isOutTime(startAt: DateTime, finishAt: DateTime): boolean {
+  return startAt.hour < 9 || finishAt.hour > 18;
+}
+export function isSameDay(date1: DateTime, date2: DateTime): boolean {
+  return date1.hasSame(date2, 'day') && date1.hasSame(date2, 'month') && date1.hasSame(date2, 'year');
+}
+export function areThereUsersUnavailable(meetingsByUser: IMeeting[], startAt: DateTime, finishAt: DateTime): boolean {
+  return meetingsByUser.some((meeting: IMeeting) => {
+    const meetingStartDataTime = parseToISODataTime(meeting.startAt);
+    const meetingFinishDataTime = parseToISODataTime(meeting.finishAt);
+    return isMeetingsInSameTime(startAt, finishAt, meetingStartDataTime, meetingFinishDataTime);
+  });
+}
+function timeOverlaps(startAtDate1: DateTime, finishAtDate1: DateTime, startAtDate2: DateTime, finishAtDate2: DateTime): boolean {
+  // empieza misma hora
+  return startAtDate1.hasSame(startAtDate2, 'minute')
+    || finishAtDate1.hasSame(finishAtDate2, 'minute')
+    // empieza antes acaba igual o después
+    || startAtDate2.diff(startAtDate1, 'hour').hours > 0
+    && startAtDate2.diff(finishAtDate1, 'hour').hours <= 0
+    // empieza en mitad de una reuniçon
+    || startAtDate2.diff(startAtDate1, 'hour').hours < 0
+    && finishAtDate2.diff(startAtDate1, 'hour').hours > 0;
+}
+function isMeetingsInSameTime(startAtDate1: DateTime, finishAtDate1: DateTime, startAtDate2: DateTime, finishAtDate2: DateTime) {
+  return isSameDay(startAtDate1, startAtDate2) && timeOverlaps(startAtDate1, finishAtDate1, startAtDate2, finishAtDate2);
+}
